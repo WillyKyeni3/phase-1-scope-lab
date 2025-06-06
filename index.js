de1e89dc17e077ec35de02ef4d19b58dcf4331b8
willymusyoka <willykyeni@gmail.com>
2025-06-06 15:03:10 +0300

    commited scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+   const upperCaseName = customerName.toUpperCase();
+   console.log ("Result for upperCaseCustomerName function: ", upperCaseName);
+   return upperCaseName;
+}
+console.log(upperCaseCustomerName());
+
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+
+const leastFavoriteCustomer = "Rose";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Jane";
+}
+console.log(changeLeastFavoriteCustomer());
